40cf430b30ea6490bdb8bfe975daf7159aa4d51b
JMHiero <106533554+JMHiero@users.noreply.github.com>
2022-11-29 22:25:00 +0100

    comments to Searchbar2.js

--- PLS Project main/Searchbar2.js
+++ PLS Project main/Searchbar2.js
@@ -1,3 +1,4 @@
+//query from wikidata
 class SPARQLQueryDispatcher {
     constructor(endpoint) {
         this.endpoint = endpoint;
@@ -14,12 +15,11 @@ const endpointUrl = 'https://query.wikidata.org/sparql';
 const sparqlQuery = `SELECT ?disease ?diseaseLabel ?symptom ?symptomLabel
 WHERE {
 ?disease wdt:P31 wd:Q18123741.
-?disease wdt:P780 ?symptom.
+?disease wdt:P780 ?symptom. 
 SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
-}`;
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
-  }
+}`; //the ?symptom only exists for the query to only return diseases that have symptoms
+
+// Called after query. Used to remove douplicates
 function Load(result) {
     var set = new Set()
     for (let i = 0; i < result.results.bindings.length; i++) {
@@ -28,26 +28,30 @@ function Load(result) {
 
     diseaseIDs = Array.from(set)
     diseases = new Array(diseaseIDs)
-    for (let j = 0; j< diseaseIDs.length;j++){
+    for (let j = 0; j< diseaseIDs.length;j++){ //probably could remove douplicates more efficiently
         for (let i = 0; i < result.results.bindings.length; i++) {
             if(result.results.bindings[i].disease.value === diseaseIDs[j]){
                 diseases[j] = { ID: result.results.bindings[i].disease.value, name: result.results.bindings[i].diseaseLabel.value }
             }
         }
     }
-    console.log(diseases)
+    //console.log(diseases)
     return diseases
 }
+
+// the actual part that runs the code
 async function main() {
     const queryDispatcher = new SPARQLQueryDispatcher(endpointUrl);
     diseases = await queryDispatcher.query(sparqlQuery).then(Load);
+
+    //Adds listener to searchbar
     const searchInput = document.querySelector('.input')
     searchInput.addEventListener("input", (e) => {
         let value = e.target.value
 
-        if (value) {
-            value = value.trim().toLowerCase()
-            //returning only the results of setDropdown if the value of the search is included in the d's name
+        if (value) { //checks if there is an entered value
+            value = value.trim().toLowerCase() 
+            //returning only the results to setDropdown if the value of the search is included in the disease's name
             setDropdown(diseases.filter(d => {
                 return d.name.includes(value)
             }))
@@ -56,38 +60,45 @@ async function main() {
             clearList()
         }
     })
-    const clearButton = document.getElementById('clear')
 
-    clearButton.addEventListener("click", () => {
-        clearList()
-    })
+    //establishes the rendom button (show example)
     const randomButton = document.getElementById('random')
-
     randomButton.addEventListener("click", () => {
-        let selected = diseases[Math.floor(Math.random() * diseases.length)].name
-        //returning only the results of setDropdown if the value of the search is included in the d's name
+        let selected = diseases[Math.floor(Math.random() * diseases.length)].name //selects a random disease
+        //filters out all other diseases
         setDropdown(diseases.filter(d => {
             return d.name.includes(selected)
         })
         )
     })
 
+    //removes all the entries in the dropdown
     function clearList() {
-        // looping through each child of the search results list and remove each child
         document.getElementById("dropdown").innerHTML = ""
     }
 
 
-
+    //generates the dropdown menu
     function setDropdown(results) {
         clearList()
         var dropdown = "<select>"
+
+        //fixes an issue with the first element being unselectable
+        if(results&&results-length>0){
+            dropdown += "<option value=\"diabled selected\">Select your Disease</option>"
+        }
+
+        //creates an option for every disease that matches the searchbar input
         for (let i = 0; i < results.length; i++) {
             dropdown += "<option value=\"" + results[i].ID + "\">" + results[i].name + "</option>"
         }
         dropdown += "</select>"
+
+        //updates the dropdown in the html
         document.getElementById("dropdown").innerHTML = dropdown
 
     }
 }
+
+// actually runs the code
 main()
